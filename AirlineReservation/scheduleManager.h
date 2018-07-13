2e20e4090532a787f0815842edd1037c6d9fc79d
Temitayo Fagbami <temitayo.fagbami@gmail.com>
2018-07-13 00:34:52 -0700

    Merge branch 'master' of https://kalteam5.visualstudio.com/AirlineReservation/_git/AirlineReservation
    from master pushed back

---
+++ AirlineReservation/scheduleManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class ScheduleManager {
+
+	
+	//int reserveFlight(Passenger p, Flight f);
+	void displayTicket(int reservationId);
+	void cancelReservationId();
+	//Ticket flightCheckIn(int reservationId);
+
+
+
+};
