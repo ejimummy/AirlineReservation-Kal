49d5597afe07115799e6f4c62cf585482434bd27
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-07-13 13:55:29 -0700

    Revert "updated Ticket header and cpp"
    This reverts commit 61158457ed277bae4d96038e2ae6793b40b2d5c3, reversing
    changes made to e956c83f229f838a64b2629c400a8bb2d3ad0605.

--- AirlineReservation/ticket.h
+++ AirlineReservation/ticket.h
@@ -1,48 +1,15 @@
 #pragma
-#include "Passenger.h"
-#include "Flight.h"
-#include "Reservation.h"
-#include<string>
-using std::string;
-using namespace std;
 
 class Ticket {
-private:
-	int mLastTicketId;
-	int mTicketId;
-	Reservation mReservation;
-	Flight mReservedFlight;
-	Passenger mPassenger;
-	string mSeatNumber;
-	//Bitmap mQrCode;
-	
 public:
-	//constructors and destructor
-	Ticket();
-	Ticket(Reservation reservation);
-	~Ticket();
+	int reservationId;
+	Passenger passenger;
+	Flight flight;
+	Bitmap qrCode;
 
-	//getters and setters
-	int getTicketId();
+	createQRCode() {
 
-	void setTicketId();
-
-	int getTicketId();
-
-	Flight getReservedFlight();
-
-	void setReservedFlight(Flight flight);
-
-	Passenger getPassenger();
-
-	void setPassenger(Passenger p);
-
-	string getSeatNumber();
-
-	void setSeatNumber(string seatNumber);
-		
-	//methods
-	/*void createQRCode(); */
+	}
 
 	
 
