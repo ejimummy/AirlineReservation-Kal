c5578c57c044ac6a8ae026acc1925e86bcfd76e4
Lisa Hare <hare.lisa@gmail.com>
2018-07-13 12:12:52 -0700

    updated ticket header and cpp parameters and function names

---
+++ AirlineReservation/scheduleManager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Reservation.h"
+#include "Passenger.h"
+#include "flight.h"
+#include "ticket.h"
+
+
+class ScheduleManager {
+
+	
+	//int reserveFlight(Passenger p, Flight f);
+	void displayTicket(int reservationId);
+	void cancelReservationId();
+	void passengerCheckin(int reservationId);
+	//Ticket flightCheckIn(int reservationId);
+
+
+
+};
--- AirlineReservation/ticket.h
+++ AirlineReservation/ticket.h
@@ -1,15 +1,48 @@
 #pragma
+#include "Passenger.h"
+#include "Flight.h"
+#include "Reservation.h"
+#include<string>
+using std::string;
+using namespace std;
 
 class Ticket {
+private:
+	int mLastTicketId;
+	int mTicketId;
+	Reservation mReservation;
+	Flight mReservedFlight;
+	Passenger mPassenger;
+	string mSeatNumber;
+	//Bitmap mQrCode;
+	
 public:
-	int reservationId;
-	Passenger passenger;
-	Flight flight;
-	Bitmap qrCode;
+	//constructors and destructor
+	Ticket();
+	Ticket(int reservationId);
+	~Ticket();
 
-	createQRCode() {
+	//getters and setters
+	int getTicketId();
 
-	}
+	void setTicketId();
+
+	int getTicketId();
+
+	Flight getReservedFlight();
+
+	void setReservedFlight(Flight flight);
+
+	Passenger getPassenger();
+
+	void setPassenger(Passenger p);
+
+	string getSeatNumber();
+
+	void setSeatNumber(string seatNumber);
+		
+	//methods
+	/*void createQRCode(); */
 
 	
 
