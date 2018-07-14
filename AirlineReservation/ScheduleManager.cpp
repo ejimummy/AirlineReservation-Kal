bfdd7391dc1acffc3b5fb56f6ec0a6cf46cc3f68
Wen <8473652+wendyscarlet@users.noreply.github.com>
2018-07-14 15:42:15 -0700

    Fixed Compiler Errors

--- AirlineReservation/ScheduleManager.cpp
+++ AirlineReservation/ScheduleManager.cpp
@@ -1,2 +1,25 @@
 #include "scheduleManager.h"
 #include "stdafx.h"
+//#include "fileHelper.h"
+#include<iostream>
+#include<string>
+using std::string;
+using namespace std;
+
+void displayTicket(int reservationId)
+{
+	//use FileHelper to retrieve the ticket that corresponds to the reservation number
+	//if no ticket exists give error, did they remember to check in?
+	//display ticket information including Name, Data, Flight#, Seat#, Destination city, and time
+}
+
+void cancelReservationId()
+{
+}
+
+void passengerCheckin(int reservationId)
+{
+	/*Ticket myTicket(reservationId);
+	cout << "Thank you for checking in. Below is your ticket to board.";
+	displayTicket(reservationId);*/
+}
--- AirlineReservation/scheduleManager.h
+++ AirlineReservation/scheduleManager.h
@@ -1,14 +1,16 @@
 #pragma once
+//#include "Reservation.h"
+//#include "flight.h"
+//#include "ticket.h"
 
 
 class ScheduleManager {
+public:
+	ScheduleManager() = default;
 
-	
 	//int reserveFlight(Passenger p, Flight f);
 	void displayTicket(int reservationId);
 	void cancelReservationId();
+	void passengerCheckin(int reservationId);
 	//Ticket flightCheckIn(int reservationId);
-
-
-
 };
