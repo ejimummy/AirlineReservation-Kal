b1b34caefe11f8e5c06b55a4bea890b2da7472b1
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-07-14 23:33:09 -0700

    updated Flight header flie and cpp file

--- AirlineReservation/flight.cpp
+++ AirlineReservation/flight.cpp
@@ -5,3 +5,122 @@
 using std::string;
 using namespace std;
 
+Flight::Flight(const int id, const time_t date, const time_t departureTime, const time_t arrivalTime, const std::string	departureGate,
+	const std::string arrivalGate, const airportsEnum departureAirport, const airportsEnum airrvalAirport)
+{
+	 mflightId = id;
+	 mdate=date;
+	 mdepartureTime=departureTime;
+	 marrivalTime=arrivalTime;
+	 mdepartureGate=departureGate;
+	 marrivalGate=arrivalGate;
+	 mdepartureAirport=departureAirport;
+	 mairrvalAirport= airrvalAirport;
+
+}
+
+void Flight::display() const
+{
+	cout << "Flight Id:        " <<mflightId << endl;
+	cout << "Date              " << mdate << endl;
+	cout << "DepartureTime:    " << mdepartureTime << endl;
+	cout << "ArrivalTime:      " << marrivalTime << endl;
+	cout << "Departure Gate:   " << mdepartureGate << endl;
+	cout << "Arrival Gate:     " << marrivalGate << endl;
+	cout << "Departure Airport:" << mdepartureAirport << endl;
+	cout << "Arrival Airport:  " << mairrvalAirport << endl;
+}
+
+void Flight::setId(int id)
+{
+	mflightId = id;
+}
+
+int Flight::getId() const
+{
+	return mflightId;
+}
+
+void Flight::setDate(const time_t date)
+{
+	mdate = date;
+}
+
+const time_t Flight::getDate() const
+{
+	return mdate;
+}
+
+void Flight::setDepartureTime(const time_t departureTime)
+{
+	mdepartureTime = departureTime;
+}
+
+const time_t Flight::getDepartureTime() const
+{
+	return mdepartureTime;
+}
+
+void Flight::setArrivalTime(const time_t arrivalTime)
+{
+	marrivalTime = arrivalTime;
+}
+
+const time_t Flight::getArrivalTime() const
+{
+	return marrivalTime;
+}
+
+void Flight::setDepartureGate(const string & departureGate)
+{
+	mdepartureGate = departureGate;
+}
+
+const string & Flight::getDepartureGate() const
+{
+	// TODO: insert return statement here
+	return mdepartureGate;
+}
+
+void Flight::setArrivalGate(const string & arrivalGate)
+{
+	marrivalGate = arrivalGate;
+}
+
+const string & Flight::getArrivalGate() const
+{
+	// TODO: insert return statement here
+	return marrivalGate;
+}
+
+void Flight::setdepartureAirport(const airportsEnum departureAirport)
+{
+	mdepartureAirport = departureAirport;
+}
+
+const Flight::airportsEnum Flight::getDepartureAirport() const
+{
+	return mdepartureAirport;
+}
+
+void Flight::setAirrvalAirport(const airportsEnum airrvalAirport)
+{
+	mairrvalAirport = airrvalAirport;
+}
+
+const Flight::airportsEnum Flight::getAirrvalAirpot() const
+{
+	return mairrvalAirport;
+}
+
+void Flight::setFlightStatus(const flightstatusEnum flightStatus)
+{
+	mflightStatus = flightStatus;
+}
+
+const Flight::flightstatusEnum Flight::getFlightStatus()
+{
+	return mflightStatus;
+}
+
+
--- AirlineReservation/flight.h
+++ AirlineReservation/flight.h
@@ -1,16 +1,49 @@
 #pragma once
 # include<string>
+# include<ctime>
 
 class Flight {
-private:
-	int flightId, cost;
-	std::string date, departureTime, arrivalTime, departureGate, arrivalGate;
-	//std::enum departureAirport, arrivalAirport, flightstatus;
-	//std::airplane airplane;
-	void createFlight();
-	void removeFlight();
-	//std::string set ArrivalGate(airportId);
-	//std::string set departureGate(airportId);
+public:
+	// enum declaration
+	enum airportsEnum { SEA, HYD, DEL, DWT, KOL, LOA };
+	enum flightstatusEnum { Delayed, Boarding, Ontime, Deparated };
 
+	// constructors
+	Flight() = default;
+	Flight(const int id, const time_t date, const time_t departureTime, const time_t arrivalTime, const std::string	departureGate,
+		const std::string arrivalGate,const airportsEnum departureAirport,const airportsEnum airrvalAirport);
+	void display() const;
+
+	// Getters and Setters
+	void setId(int id);
+	int getId() const;
+	void setDate(const time_t date);
+	const time_t getDate() const;
+	void setDepartureTime(const time_t departureTime);
+	const time_t getDepartureTime() const;
+	void setArrivalTime(const time_t arrivalTime);
+	const time_t getArrivalTime() const;
+	void setDepartureGate(const string& departureGate);
+	const string& getDepartureGate() const;
+	void setArrivalGate(const string& arrivalGate);
+	const string& getArrivalGate() const;
+	void setdepartureAirport(const airportsEnum departureAirport);
+	const airportsEnum getDepartureAirport() const;
+	void setAirrvalAirport(const airportsEnum airrvalAirport);
+	const airportsEnum  getAirrvalAirpot() const;
+	void setFlightStatus(const flightstatusEnum flightStatus);
+	const flightstatusEnum  getFlightStatus();
+
+private:
+	int mflightId=-1;
+	time_t mdate;
+	time_t mdepartureTime;
+	time_t marrivalTime;
+	std::string	mdepartureGate;
+	std::string marrivalGate;
+	airportsEnum mdepartureAirport;
+	airportsEnum mairrvalAirport;
+	flightstatusEnum mflightStatus;
+	//airplane airplane;
 
 };
