ce36ff7f4ee35ae7240efef1f4b81baa7bf1cfa5
WenScarlet <8473652+wendyscarlet@users.noreply.github.com>
2018-07-17 16:01:13 -0700

    Merge of ScheduleManager

--- AirlineReservation/ScheduleManager.cpp
+++ AirlineReservation/ScheduleManager.cpp
@@ -1,12 +1,16 @@
 #include "stdafx.h"
 #include "scheduleManager.h"
-#include<iostream>
-#include<string>
-#include <vector>
 #include "flight.h"
 #include "Passenger.h"
 #include "Reservation.h"
+#include<iostream>
+#include<string>
+#include <vector>
 #include<unordered_map>
+#include<ctime>
+#include <fstream>
+#include<conio.h>
+#include<iomanip>
 
 using std::string;
 using namespace std;
@@ -42,7 +46,6 @@ namespace AirlineReservation {
 		}
 
 	}
-
 
 	void ScheduleManager::cancelReservationId()
 	{
@@ -143,5 +146,110 @@ namespace AirlineReservation {
 		return r;
 	}
 
+
+	void ScheduleManager::addFlightInformation() {
+		string tempDate, departureAirport, arrivalAirport, arrivalTime, departureTime, origin, destination, flightNumber;
+		cout << "Enter Flight Number" << endl;
+		cin >> flightNumber;
+		cout << "Enter Origin " << endl;
+		cin >> origin;
+		cout << "Enter Destination " << endl;
+		cin >> destination;
+		cout << "Enter Date(MM/DD/YYYY)" << endl;
+		cin >> tempDate;
+		cout << "Enter Departure Time(HH:MM AM/PM)" << endl;
+		cin >> departureTime;
+		cout << "Enter Arrival Time(HH:MM AM/PM)" << endl;
+		cin >> arrivalTime;
+		cout << "Enter Departure airport " << endl;
+		cin >> departureAirport;
+		cout << "Enter Arrival airport " << endl;
+		cin >> arrivalAirport;
+
+		//cout
+		Flight theFlight;
+		//Flight theFlight(1,tempDate, "9:00PM", "11:00Am", "2A", "2B","la","ca");
+		//theFlight.setAirrvalAirport(airportsEnum :: SEA);
+		theFlight.setFlightId(mAllFlights.size() + 1);
+		theFlight.setFlightNumber(flightNumber);
+		theFlight.setDate(tempDate);
+		theFlight.setdepartureAirport(departureAirport);
+		theFlight.setAirrvalAirport(arrivalAirport);
+		theFlight.setArrivalTime(arrivalTime);
+		theFlight.setDepartureTime(departureTime);
+		theFlight.setOrigin(origin);
+		theFlight.setDestination(destination);
+
+		theFlight.addFlight();
+		mAllFlights.push_back(theFlight);
+		//writeFlightVectorToFile(mAllFlights);
+
+	}
+
+	void ScheduleManager::deleteFlight(int flightId)
+	{
+		if (mAllFlights.size() <= 0) {
+			cout << "There are no flights to delete" << endl;
+			return;
+		}
+		for (unsigned int i = 0; i < mAllFlights.size(); i++) {
+			if (mAllFlights[i].getFlightId() == flightId) {
+				mAllFlights.erase(mAllFlights.begin() + i);
+				cout << "Flight with FlightId: " << flightId << " was successfully deleted" << endl;
+				return;
+			}
+		}
+		cout << "This flight id doesn't exist!" << endl;
+		cout << "To go back to main menu enter'0'" << endl;
+		cout << "Please enter the correct FlightId" << endl;
+
+		cin >> flightId;
+		if (flightId == 0)
+			return;
+		else
+			deleteFlight(flightId);
+
+
+	}
+
+	void ScheduleManager::dummyFlightInformation() {
+		Flight theFlight(1, "10/08/2018", "10:10 AM", "9:00PM", "2A", "2B", "SEA", "LA");
+		theFlight.setFlightId(mAllFlights.size() + 1);
+		theFlight.addFlight();
+		mAllFlights.push_back(theFlight);
+	}
+
+	//Dsiplay all Flight Information
+	void ScheduleManager::displayAllFlights() const {
+		//line();
+		cout << setw(49) << "DISPALY FLIGHTS INFORMATION \n";
+		//line();
+		cout << setw(8) << "FlightId" << setw(15) << "Date" << setw(15) << "Depart Time"
+			<< setw(15) << "ArrivalTime" << setw(15) << "DepartGate" << setw(15) << "Arrival Gate" << setw(15) << "Dapart_Airport" << setw(20) << "Arrival Airport\n";
+		//line();
+		for (const auto& flight : mAllFlights) {
+			flight.displayFlight();
+		}
+		//readFlightVectorFromFile();
+	}
+
+	void ScheduleManager::writeFlightVectorToFile(vector<Flight> allFlights) {
+		fstream myfile;
+		myfile.open("Flight.txt", ios::out);
+		myfile.write((char*)&allFlights, sizeof(allFlights));
+
+		myfile.seekg(0);
+		myfile.close();
+	}
+	void ScheduleManager::readFlightVectorFromFile() const {
+		vector<Flight> allFlights;
+		fstream myfile;
+		myfile.open("Flight.txt", ios::in);
+		myfile.read(reinterpret_cast<char *>(&allFlights), sizeof(mAllFlights));
+		for (unsigned int i = 0; i < allFlights.size(); i++) {
+			allFlights[i].displayFlight();
+		}
+
+	}
 }
 
--- AirlineReservation/scheduleManager.h
+++ AirlineReservation/scheduleManager.h
@@ -8,6 +8,7 @@ namespace AirlineReservation {
 
 	private:
 		std::unordered_map<int, Ticket> ALLTickets;
+		std::vector<Flight> mAllFlights;
 
 	public:
 		ScheduleManager() = default;
@@ -17,5 +18,16 @@ namespace AirlineReservation {
 		void cancelReservationId();
 		void passengerCheckin(int reservationId);
 		//Ticket flightCheckIn(int reservationId);
+
+		//Flight:
+		//Display All Flights Information
+		void displayAllFlights() const;
+		void addFlightInformation();
+		void deleteFlight(int flightId);
+		void dummyFlightInformation();
+
+		//File - Read and Write Functions
+		void writeFlightVectorToFile(std::vector<Flight> allFlights);
+		void readFlightVectorFromFile() const;
 	};
 }
